0c3f7ae5d0c80a08097f8b5ec490957a6dc6c597
kchaurasia <kchaurasia@PF1C1A1B.ad.wcpss.net>
2022-01-19 18:03:07 -0500

    Uploading Project

---
+++ src/Practise/FirstClass.java
@@ -0,0 +1,24 @@
+package Practise;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		String name = "Kirtan";
+		String lName = "Chaurasia";
+		int salary = 6500;
+		int tax = 350;
+		int insu = 280;
+		int incent = 1200;
+		int totSalary = salary - tax - insu + incent;
+		
+		System.out.println("Employee Name : " +name +" " +lName);
+		System.out.println("Salary        : " +salary);
+		System.out.println("Tax           : " +tax);
+		System.out.println("Insurance     : " +insu);
+		System.out.println("Incentive     : " +incent);
+		System.out.println("Total Salary  : " +totSalary);
+
+	}
+
+}
